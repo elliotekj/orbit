3cdf20c9f71d7aa0a0f2e22a376cd6ed1e62050d
Elliot Jackson <elliotekj@gmail.com>
2017-12-14 17:26:33 +0100

    Cleanup
    — Elliot Jackson <elliot@elliotekj.com>

--- app/orbit.rb
+++ app/orbit.rb
@@ -13,7 +13,7 @@ require_relative 'orbit/servlet.rb'
 options = {}
 
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: #$0 -s '/path/to/hugo/site' [options]"
+  opts.banner = "Usage: #{$PROGRAM_NAME} -s '/path/to/hugo/site' [options]"
 
   options['src_path'] = nil
   opts.on('-s', '--src-path FOLDER', 'Path to your Hugo site (required)') do |s|
@@ -21,7 +21,8 @@ optparse = OptionParser.new do |opts|
   end
 
   options['content_folder'] = 'post'
-  opts.on('-c', '--content-folder FOLDER_NAME', "Name of the folder in `/content` you want Orbit to serve (default: 'post')") do |c|
+  opts.on('-c', '--content-folder FOLDER_NAME', "Name of the folder in \
+          `/content` you want Orbit to serve (default: 'post')") do |c|
     options['content_folder'] = c
   end
 
@@ -36,7 +37,8 @@ optparse = OptionParser.new do |opts|
   end
 
   options['update_command'] = nil
-  opts.on('-u', '--update-command COMMAND', 'Command run when your site is updated (optional)') do |u|
+  opts.on('-u', '--update-command COMMAND', 'Command run when your site is \
+          updated (optional)') do |u|
     options['update_command'] = u
   end
 end
@@ -44,22 +46,9 @@ end
 optparse.parse!
 
 if options['src_path'].nil? or not File.directory? options['src_path']
-    puts 'option --src-path is required must be an existing folder'
-    exit 1
+  puts 'option --src-path is required must be an existing folder'
+  exit 1
 end
-
-# puts "👋  Welcome to Orbit!\n\n"
-# puts "Please enter the path to your Hugo site (e.g. '/Users/elliot/Sites/elliotekj.com'):\n"
-# options['src_path'] = gets.chomp
-# puts "And the port would you like to run Orbit on (e.g. '4040'):\n"
-# options['port'] = gets.chomp
-# puts "And the content folder the posts you want serve live in (e.g. 'post' if you want to serve posts in 'content/post'):\n"
-# options['content_folder'] = gets.chomp
-# puts "And the token you would like to use to secure access to the endpoint?\n"
-# options['token'] = gets.chomp
-# puts "And a command to run when your site is updated? (tip: Use this to auto-regenerate your site)\n"
-# options['update_command'] = gets.chomp
-# puts '🕐  Thanks, Orbit is setting up…'
 
 db = OrbitDB.new(options)
 metaweblog_api = MetaWeblogAPI.new(db, options['update_command'])
@@ -70,11 +59,10 @@ servlet.add_handler('metaWeblog', metaweblog_api)
 servlet.add_handler('blogger', blogger_api)
 
 server = WEBrick::HTTPServer.new(:Port => options['port'])
-server.mount('/xmlrpc.php', servlet)
+server.mount('/xmlrpc', servlet)
 
-['INT', 'TERM', 'HUP'].each { |signal|
+['INT', 'TERM', 'HUP'].each do |signal|
   trap(signal) { server.shutdown }
-}
+end
 
-# puts "🚀  Orbit is running on http://localhost:#{options['port']}/xmlrpc.php!"
 server.start
